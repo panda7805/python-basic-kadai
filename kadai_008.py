9be11ceb650dba323b189f0e235ecbfc8ffe0251
panda7805 <kn7805@icloud.com>
2023-08-22 22:42:28 +0900

    Update kadai_008.py

--- kadai_008.py
+++ kadai_008.py
@@ -2,14 +2,14 @@ var=int(input("数字を入力してください:"))
 
 var>0
 
-if var%3==0:
+if var % 3 == 0 and var % 5 == 0:
+  print("FizzBuzz")
+   
+elif var%3==0:
   print("Fizz")
 
 elif var%5==0:
   print("Buzz")
-
-elif var % 15==0: 
-  print("FizzBuzz")
 
 else:
   print(var)
