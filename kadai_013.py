a3128fdc730e5c6313b02bc2fb289bbabae5e430
panda7805 <kn7805@icloud.com>
2023-08-27 11:19:31 +0900

    Update kadai_013.py

--- kadai_013.py
+++ kadai_013.py
@@ -2,5 +2,6 @@ def add_arguments(sum,tax):
   total=sum+tax
   return (total)
   #第1引数に商品の金額、第2引数に消費税
-  result=add_arguments(5000,500)
+  add_arguments(300,30)
+  result=add_arguments(300,30)
 print("商品の合計金額は",result,"です。")
